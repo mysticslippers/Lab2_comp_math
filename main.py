58d90f20c70a73de56d468b170ea909c589d5886
misticslippers <eternal.martyr13579@gmail.com>
2025-03-26 12:07:46 +0300

    add chord method

--- main.py
+++ main.py
@@ -32,3 +32,39 @@ def iteration_method(x0, f, e, maxitr=100):
         itr += 1
 
     return x, f(x), itr, log
+
+def chord_method(a, b, f, e, maxitr=100):
+    """ Метод хорд """
+    log = [['a', 'b', 'x', 'f(a)', 'f(b)', 'f(x)', '|x - x0|']]
+
+    if f(a) * d(2, a, f) < 0:
+        x = a
+        fix_x = b
+    elif f(b) * d(2, a, f) < 0:
+        x = b
+        fix_x = a
+    else:
+        x = a - (b - a) / (f(b) - f(a)) * f(a)
+        fix_x = None
+
+    x0 = x + 2 * e
+    log.append([a, b, x, f(a), f(b), f(x), abs(x - x0)])
+
+    itr = 0
+    while abs(x - x0) > e and itr < maxitr:
+        if fix_x is None:
+            if f(a) * f(x) < 0:
+                b = x
+            else:
+                a = x
+            x, x0 = a - (b - a) / (f(b) - f(a)) * f(a), x
+            log.append([a, b, x, f(a), f(b), f(x), abs(x - x0)])
+        else:
+            x, x0 = x - (fix_x - x) / (f(fix_x) - f(x)) * f(x), x
+            if fix_x == a:
+                log.append([fix_x, x, x, f(fix_x), f(x), f(x), abs(x - x0)])
+            else:
+                log.append([x, fix_x, x, f(x), f(fix_x), f(x), abs(x - x0)])
+        itr += 1
+
+    return x, f(x), itr, log
