00d81dddd1668738001c07a95c5c73713925c6c1
misticslippers <eternal.martyr13579@gmail.com>
2025-03-26 12:06:56 +0300

    add iteration method

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ def derivative(n, x, f, h=0.00000001):
         return (f(x + h) - f(x)) / h
 
     return (derivative(n - 1, x + h, f) - derivative(n - 1, x, f)) / h
+
+def iteration_method(x0, f, e, maxitr=100):
+    """ Метод простой итерации """
+    log = [['x0', 'f(x0)', 'x', 'g(x0)', '|x - x0|']]
+
+    def g(g_x):
+        return g_x + (-1 / derivative(1, g_x, f)) * f(g_x)
+
+    x = g(x0)
+    log.append([x0, f(x0), x, g(x0), abs(x - x0)])
+
+    itr = 0
+    while abs(x - x0) > e and itr < maxitr:
+        if derivative(1, x, g) >= 1:
+            return None
+        x0, x = x, g(x)
+        log.append([x0, f(x0), x, g(x0), abs(x - x0)])
+        itr += 1
+
+    return x, f(x), itr, log
