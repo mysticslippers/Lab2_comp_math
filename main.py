b8dc2678334ee56e6ac717155222318ed73b1786
misticslippers <eternal.martyr13579@gmail.com>
2025-04-09 18:44:39 +0300

    add get_func

--- main.py
+++ main.py
@@ -24,3 +24,14 @@ def plot(x, y):
 
     plt.plot(x, y)
     plt.show(block=False)
+
+
+def get_func(function_num):
+    """ Получить выбранную функцию. """
+    functions = {
+        '1': (np.linspace(-1, 3, 200), lambda x: x ** 3 - 2.92 * (x ** 2) + 1.435 * x + 0.791),
+        '2': (np.linspace(-3, 2, 200), lambda x: x ** 3 - x + 4),
+        '3': (np.linspace(-20, 20, 200), lambda x: np.sin(x) + 0.1)
+    }
+
+    return functions.get(function_num, (None, None))
